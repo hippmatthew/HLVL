601d4e76dbb2693bc2023793c226fb08a1e1802f
Matthew Hipp <74476804+hippmatthew@users.noreply.github.com>
2024-08-31 16:03:03 -0500

    Physics+ 0.62.1
    Forgot to update physics+ version

--- src/core/include/settings_decl.hpp
+++ src/core/include/settings_decl.hpp
@@ -19,7 +19,7 @@
   ((patch))         \
 )
 #define pp_vulkan_version   VK_MAKE_API_VERSION(0, 1, 3, 290)
-#define pp_engine_version   pp_make_version(0, 56, 0)
+#define pp_engine_version   pp_make_version(0, 62, 1)
 
 #define pp_settings_manager pp::SettingsManager::instance()
 #define pp_general_settings pp_settings_manager.settings<pp::GeneralSettings>()
